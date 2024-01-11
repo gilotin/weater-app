57c1e93d9550a0a5a57058a7b42b24ef7b97e4c7
Nikolay <103648794+gilotin@users.noreply.github.com>
2024-01-11 15:44:09 +0200

    added styles to the main app

--- src/app/weatherApp/page.js
+++ src/app/weatherApp/page.js
@@ -5,9 +5,12 @@ import { API_KEY } from "@/constants";
 import style from "../weatherApp/page.module.css";
 import dynamic from "next/dynamic";
 
-const Clock = dynamic(() => import("../components/Clock/Clock").then((module) => module.Clock), {
-    ssr: false,
-});
+const Clock = dynamic(
+    () => import("../components/forecastComponent/Clock/Clock").then((module) => module.Clock),
+    {
+        ssr: false,
+    }
+);
 
 export function WeatherApp() {
     const [city, setCity] = useState("");
@@ -37,11 +40,58 @@ export function WeatherApp() {
 
     return (
         <>
-            <section className={style.timer}>
-                <Clock />
-            </section>
-
-            <h1>Hello to my test weather app</h1>
+            <div className={style.app__wrapper}>
+                <section className={style.main__section}>
+                    <div className={style.main__info}>
+                        <h1 className={style.clock}>
+                            <Clock />
+                        </h1>
+                        <p className={style.paragraph}>WEDNESDAY</p>
+                        <p className={style.paragraph}>MAY 23</p>
+                        <h2 className={style.currentTemp}>{cityData.current?.temp_c}</h2>
+                        <p className={style.paragraph}>{cityData.location?.name}</p>
+                        <p className={style.paragraph}>Humidity 61%</p>
+                    </div>
+                    <div className={style.main__icon}>
+                        <img src="" alt="weather icon" />
+                    </div>
+                </section>
+                <section className={style.forecast}>
+                    <div className={style.forecast__column}>
+                        <div className="forecast__info">
+                            <p>MON</p>
+                            <p>16</p>
+                        </div>
+                        <div className={style.forecast__icon}>
+                            <img src="" alt="weather icon" />
+                        </div>
+                    </div>
+                    <div className={[style.forecast__grid]}>
+                        <div className={style.forecast__info}>
+                            <p>TUE</p>
+                            <p>26</p>
+                        </div>
+                        <div className={style.forecast__info}>
+                            <img src="" alt="weather icon" />
+                        </div>
+                    </div>
+                    <div className="forecast__column">
+                        <div className={style.forecast__info}>
+                            <p>WED</p>
+                            <p>20</p>
+                        </div>
+                        <div className={style.forecast__icon}>
+                            <img src="" alt="weather icon" />
+                        </div>
+                    </div>
+                </section>
+                <footer>
+                    <p className={style.app_footer}>Inspired by Freepik designs </p>
+                    <a className={style.app__settings} href="">
+                        <i className="fa-solid fa-gear"></i>
+                    </a>
+                </footer>
+            </div>
 
             <form onSubmit={handleSubmit}>
                 <label htmlFor="city">Search for your city : </label>
