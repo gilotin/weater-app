1cec245f258dc24db140a392fed5f83a0fbb20b2
Nikolay <103648794+gilotin@users.noreply.github.com>
2024-02-22 17:09:05 +0200

    fixing minor issues

--- src/app/components/forecastComponent/ForecastFront.js
+++ src/app/components/forecastComponent/ForecastFront.js
@@ -1,58 +1,58 @@
-import { useState } from "react";
-import { API_KEY } from "@/constants";
+// import { useState } from "react";
+// import { API_KEY } from "@/constants";
 
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 
-const Clock = dynamic(() => import("./Clock/Clock").then((module) => module.Clock), {
-    ssr: false,
-});
+// const Clock = dynamic(() => import("./Clock/Clock").then((module) => module.Clock), {
+//     ssr: false,
+// });
 
-export function ForecastFront() {
-    const [city, setCity] = useState("");
-    const [cityData, setCityData] = useState("");
+// export function ForecastFront() {
+//     const [city, setCity] = useState("");
+//     const [cityData, setCityData] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
 
-        const url = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=3`;
-        try {
-            const response = await fetch(url, {
-                headers: {
-                    "Content-type": "application/json",
-                },
-            });
+//         const url = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=3`;
+//         try {
+//             const response = await fetch(url, {
+//                 headers: {
+//                     "Content-type": "application/json",
+//                 },
+//             });
 
-            if (!response.ok) {
-                throw new Error(`Error: ${response.status}`);
-            }
-            const data = await response.json();
-            console.log(data);
-            setCityData(data);
-        } catch (error) {
-            console.error(error.message);
-        }
-    };
+//             if (!response.ok) {
+//                 throw new Error(`Error: ${response.status}`);
+//             }
+//             const data = await response.json();
+//             console.log(data);
+//             setCityData(data);
+//         } catch (error) {
+//             console.error(error.message);
+//         }
+//     };
 
-    return (
-        <>
-            <section className={style.timer}>
-                <Clock />
-            </section>
+//     return (
+//         <>
+//             <section className={style.timer}>
+//                 <Clock />
+//             </section>
 
-            <h1>Hello to my test weather app</h1>
+//             <h1>Hello to my test weather app</h1>
 
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="city">Search for your city : </label>
-                <input type="text" id="city" onChange={(e) => setCity(e.target.value)} />
-                <button type="submit">SEARCH</button>
-            </form>
+//             <form onSubmit={handleSubmit}>
+//                 <label htmlFor="city">Search for your city : </label>
+//                 <input type="text" id="city" onChange={(e) => setCity(e.target.value)} />
+//                 <button type="submit">SEARCH</button>
+//             </form>
 
-            <p>Country:{cityData.location?.country}</p>
-            <p>City:{cityData.location?.name}</p>
-            <p>date:{cityData.current?.last_updated}</p>
-            <p>Weather Condition:{cityData.current?.condition.text}</p>
-            <img src={cityData.current?.condition.icon} alt="sun" />
-            <p>Temperature:{cityData.current?.temp_c} Celsius</p>
-        </>
-    );
-}
+//             {/* <p>Country:{cityData.location?.country}</p>
+//             <p>City:{cityData.location?.name}</p>
+//             <p>date:{cityData.current?.last_updated}</p>
+//             <p>Weather Condition:{cityData.current?.condition.text}</p>
+//             <img src={cityData.current?.condition.icon} alt="sun" />
+//             <p>Temperature:{cityData.current?.temp_c} Celsius</p> */}
+//         </>
+//     );
+// }
--- src/app/weatherApp/WeatherConditions.js
+++ src/app/weatherApp/WeatherConditions.js
@@ -7,5 +7,5 @@ export default function WeatherIcon({ condition, isDay }) {
         customIcon = `/icons/Night_${condition}.png`;
     }
 
-    return <Image src={customIcon} width={150} height={150} alt="condition icon" />;
+    return <Image src={customIcon} width={150} height={150} alt="condition icon" priority={true} />;
 }
